57545575395cbabbaa38e4358112854ca3391659
jeonggyu-kang <kamgjk@gmail.com>
2022-04-24 20:01:41 +0900

    update_configuration

---
+++ train.py
@@ -0,0 +1,13 @@
+# train process main script
+
+from config import get_hyperparameters
+
+def main():
+    args = get_hyperparameters()
+
+    print (args)
+
+
+if __name__ == '__main__':
+    main()
+
