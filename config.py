1657b492627f385c9288534477ea30a0c7bad621
jeonggyu-kang <kamgjk@gmail.com>
2022-06-12 19:47:33 +0900

    restore files

---
+++ config.py
@@ -0,0 +1,95 @@
+# Hyperparameter control
+import os, shutil
+
+def get_hyperparameters(config = None):
+    if config is None: # Train
+        _save()
+    else: # test
+        _load(config)
+
+    ret = {}
+    ret.update( model_dict )         # updata: function for updating dictionary
+    ret.update( data_dict )
+
+    return ret
+
+# model-related params
+model_dict = dict(                  
+    name = [ 'vgg16' ],
+    imagenet_pretrained = True,
+    n_class = 5,
+    max_epoch = 100,
+    learning_rate = 1e-4,
+    # mile_stone = None,
+    mile_stone = [80, 90],
+    decay_rate = 0.1,
+    loss = 'ce',   # cross-entropy (classification)
+    #loss = 'mse',    # mean squared error (regresion)
+    image_size = (448,448),   # width, height
+    extra = ['gradcam_test']
+)
+
+train_pipeline = [
+    dict(
+        type = 'Resize',
+        width = 448,
+        height = 448
+    ),
+    dict(
+        type= 'ToTensor'
+    ),
+]
+
+test_pipeline = [
+    dict(
+        type = 'Resize',
+        width = 448,
+        height = 448
+    ),
+    dict(
+        type= 'ToTensor'
+    ),
+]
+
+
+# dataset-related params
+data_dict = dict(
+    dataset = 'CoronaryArteryDataset',
+    #dataset = 'AGEDataset',
+    save_root = './work_dir',
+    batch_size = 8,
+    workers_per_gpu = 1,
+
+    train = dict(
+        img_dir = '/home/compu/Projects/project_classifiers/data',
+        ann_file = '/home/compu/Projects/project_classifiers/data/train_dataset.parquet',
+        pipeline = train_pipeline
+    ),
+    test = dict(
+        img_dir = '/home/compu/Projects/project_classifiers/data',
+        ann_file = '/home/compu/Projects/project_classifiers/data/test_dataset.parquet',
+        pipeline = test_pipeline
+    ),
+)
+
+def _save():
+    model_version = []
+    for k in ['name', 'imagenet_pretrained', 'extra']:
+        if k in model_dict:
+            if isinstance(model_dict[k], list):
+                model_version += model_dict[k]
+            else:
+                model_version.append(str(model_dict[k]))
+    
+    os.makedirs(data_dict['save_root'], exist_ok = True)
+    VERSION = '.'.join(model_version)
+    VERSION = str('{:04d}'.format(len(os.listdir(data_dict['save_root'])) + 1) + '_') + VERSION
+
+    SAVE_ROOT_DIR = os.path.join(data_dict['save_root'], VERSION)
+    os.makedirs(SAVE_ROOT_DIR, exist_ok=True)
+    shutil.copy2(os.path.abspath(__file__), os.path.join(SAVE_ROOT_DIR, __name__ + '.py'))
+    data_dict['save_root'] = SAVE_ROOT_DIR
+
+def _load(config):
+    data_dict['save_root'] = os.path.join(os.path.dirname(config), 'eval')
+    data_dict['max_epoch'] = None
---
+++ test.py
@@ -0,0 +1,73 @@
+import os 
+import copy 
+import argparse 
+
+from utils import parse_args_from_config 
+from models import get_model 
+from logger import get_logger 
+from runner import tester 
+from dataset import get_dataloader
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--log_root', type=str, default=None, help='Path to log directory')
+    _args = parser.parse_args()
+
+    if not os.path.exists(_args.log_root):
+        print('Can not find directory {}'.format(_args.log_root))
+        exit()
+
+
+    _args.config = os.path.join(_args.log_root, 'config.py')
+    modulevar = parse_args_from_config(_args.config)
+
+    args = modulevar.get_hyperparameters(config=_args.config)
+
+
+    for model_name in args['name']:              # loop in various models
+        log_root = os.path.join(_args.log_root, model_name)
+
+        ckpt_path = os.path.join(log_root, 'best.pt')
+        input_size = args['image_size']
+        model = get_model(model_name, args['n_class'], input_size = (2,3, *input_size), ckpt_path = ckpt_path)
+
+        if args['loss'] in ['ce', 'cross-entropy']:
+            task_type = 'classification'
+
+        elif args['loss'] in ['mse', 'mean_squared_error']:
+            task_type = 'regression'
+
+        else:
+            raise ValueError
+
+
+        mode = 'test'
+        test_loader = get_dataloader(
+            dataset = args['dataset'],
+            data_dir = args[mode]['img_dir'],
+            ann_path = args[mode]['ann_file'],
+            mode = mode,
+            batch_size = args['batch_size'],
+            num_workers = args['workers_per_gpu'],
+            pipeline = args[mode]['pipeline']
+        )
+
+        #writer = get_logger(args['save_root'] )
+
+        save_path = os.path.join(log_root, 'eval')
+        writer = get_logger(save_path)
+
+
+        tester(
+            model = model,    
+            test_loader = test_loader,
+            writer = writer,
+            hard_sample_mining = True,
+            confusion_matrix = True,
+            n_class = args['n_class'],
+            task_type = task_type
+        )
+
+
+if __name__ == '__main__':
+    main()
